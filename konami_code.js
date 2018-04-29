f84012d32d8b5c55be2225ac40def4f8491507f6
Douglas Craig <dcraig112@gmail.com>
2018-04-29 15:07:34 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,17 +1,1 @@
 
-
-function init(keydown) {
-  const key = parseInt(keydown.detail || keydown.which);
-
-  if (key === code[index]) {
-    index++;
-
-    if (index === code.length) {
-      alert("Hurray!");
-
-      index = 0;
-    }
-  } else {
-    index = 0;
-  }
-}
