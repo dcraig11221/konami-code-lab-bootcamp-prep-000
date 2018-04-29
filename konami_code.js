d906e6cb251d9061955b09b244d2bf1524f01783
Douglas Craig <dcraig112@gmail.com>
2018-04-29 15:35:16 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,6 @@
 let index = 0
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-
-document.addEventListener('keydown', function(keydown) {
+function init(keydown) {
   let key = parseInt(keydown.detail || keydown.which);
   if (key === code[index]) {
     index++;
@@ -13,4 +12,6 @@ document.addEventListener('keydown', function(keydown) {
   else {
     index = 0;
   }
-})
+}
+
+document.addEventListener('keydown', )
